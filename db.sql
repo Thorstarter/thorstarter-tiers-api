58640d2f1ff24033556b36e82c448ef6118257ce
kiasaki <kiasaki0000@gmail.com>
2022-01-14 15:23:36 -0500

    user profiles

--- db.sql
+++ db.sql
@@ -17,3 +17,43 @@ create table kyc (
   verified boolean not null default false,
   created_at timestamptz not null default now()
 );
+
+create table users (
+  id text primary key,
+  kyc_verified boolean not null default false,
+  address_ethereum text not null,
+  address_terra text not null,
+  address_fantom text not null,
+  address_polygon text not null,
+  amount_ethereum int not null,
+  amount_terra int not null,
+  amount_fantom int not null,
+  amount_polygon int not null,
+  amount_tclp int not null,
+  amount_forge int not null,
+  iphash text not null,
+  created_at timestamptz not null default now(),
+  updated_at timestamptz not null default now()
+);
+
+create table users_addresses (
+  id text primary key,
+  user_id text not null,
+  network text not null,
+  address text not null,
+  created_at timestamptz not null default now()
+);
+
+create table users_ips (
+  id text primary key,
+  user_id text not null,
+  iphash text not null,
+  created_at timestamptz not null default now()
+);
+
+create table users_registrations (
+  id text primary key,
+  ido text not null,
+  user_id text not null,
+  created_at timestamptz not null default now()
+);
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"math/big"
 	"net"
 	"net/http"
 	"os"
@@ -20,8 +22,12 @@ import (
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rpc"
 )
+
+const allIdos = "mnet,luart,ring"
+const networks = "ethereum,terra,fantom,polygon,solana"
 
 const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
 const contractTiersAddress = "0x817ba0ecafD58460bC215316a7831220BFF11C80"
@@ -46,88 +52,6 @@ var idoTiersMul = map[string][]float64{
 
 type J map[string]interface{}
 
-func Check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func Getenv(key string, alt string) string {
-	if v := os.Getenv(key); v != "" {
-		return v
-	}
-	return alt
-}
-
-func MustParseInt(s string) int {
-	i, err := strconv.Atoi(s)
-	Check(err)
-	return i
-}
-
-func SUUID() string {
-	u := [16]byte{}
-	_, err := rand.Read(u[:16])
-	if err != nil {
-		panic(err)
-	}
-	return fmt.Sprintf("%x", u)
-}
-
-func RenderJson(w http.ResponseWriter, v interface{}) {
-	bs, err := json.Marshal(v)
-	if err != nil {
-		w.WriteHeader(500)
-		RenderJson(w, J{"error": err.Error()})
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(bs)
-}
-
-func ReqBody(r *http.Request, v interface{}) {
-	bs, err := io.ReadAll(r.Body)
-	Check(err)
-	r.Body.Close()
-	Check(json.Unmarshal(bs, &v))
-}
-
-func ReqIP(r *http.Request) string {
-	ip := r.Header.Get("X-REAL-IP")
-	nip := net.ParseIP(ip)
-	if nip != nil {
-		return ip
-	}
-
-	ips := r.Header.Get("X-FORWARDED-FOR")
-	sips := strings.Split(ips, ",")
-	for _, ip := range sips {
-		nip := net.ParseIP(ip)
-		if nip != nil {
-			return ip
-		}
-	}
-
-	ip, _, err := net.SplitHostPort(r.RemoteAddr)
-	Check(err)
-	nip = net.ParseIP(ip)
-	if nip != nil {
-		return ip
-	}
-	return ""
-}
-
-func DbSelect(sql string, args ...interface{}) []J {
-	results := []J{}
-	rows, err := db.Queryx(sql, args...)
-	Check(err)
-	for rows.Next() {
-		result := make(map[string]interface{})
-		Check(rows.MapScan(result))
-		results = append(results, result)
-	}
-	return results
-}
-
 var db *sqlx.DB
 var client *rpc.Client
 var contractTiers abi.ABI
@@ -137,46 +61,186 @@ func main() {
 	db, err = sqlx.Open("postgres", Getenv("DATABASE_URL", "postgres://admin:admin@localhost/ts_tiers_api?sslmode=disable"))
 	Check(err)
 
-	client, err = rpc.DialHTTP(Getenv("ETH_RPC", "https://main-light.eth.linkpool.io/"))
+	client, err = rpc.DialHTTP(Getenv("ETH_RPC", "https://cloudflare-eth.com/"))
 	Check(err)
 	contractTiers, err = abi.JSON(strings.NewReader(contractTiersABI))
 	Check(err)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/stats", handleStats)
-	mux.HandleFunc("/user", handleUser)
-	mux.HandleFunc("/register", handleRegister)
-	mux.HandleFunc("/kyc-start", handleKycStart)
+	mux.HandleFunc("/user-fetch", handleUserFetch)
+	mux.HandleFunc("/user-register", handleUserRegister)
+
+	//mux.HandleFunc("/stats", handleStats)
+	//mux.HandleFunc("/user", handleUser)
+	//mux.HandleFunc("/register", handleRegister)
+	//mux.HandleFunc("/kyc-start", handleKycStart)
 	//mux.HandleFunc("/admin/bots", handleAdminBots)
 	//mux.HandleFunc("/admin/kill-bots", handleAdminKillBots)
-	mux.HandleFunc("/admin/snapshot", handleAdminSnapshot)
+	//mux.HandleFunc("/admin/snapshot", handleAdminSnapshot)
 	mux.HandleFunc("/", handleIndex)
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("request", r.Method, r.URL.String())
-		defer func() {
-			if err := recover(); err != nil {
-				fmt.Println("error", err)
-				w.WriteHeader(500)
-				RenderJson(w, J{"error": fmt.Sprintf("%v", err)})
-			}
-		}()
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(200)
-			return
-		}
-		mux.ServeHTTP(w, r)
-	}
+	handler := middleware(mux.ServeHTTP)
 
 	port := Getenv("PORT", "8000")
 	log.Println("starting on port", port)
 	log.Fatal(http.ListenAndServe(":"+port, http.HandlerFunc(handler)))
 }
 
+// HANDLERS
+///////////////////////////////////////////////////////////////
+
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	RenderJson(w, J{"message": "thorstarter tiers api"})
+	RenderJson(w, 200, J{"message": "thorstarter tiers api"})
+}
+
+func handleUserFetch(w http.ResponseWriter, r *http.Request) {
+	iphash := fmt.Sprintf("%x", sha256.Sum256([]byte(ReqIP(r))))
+	user := J{"id": SUUID(), "updated_at": time.Now().Add(-1 * time.Hour)}
+	refresh := r.URL.Query().Get("refresh") == "1"
+	address := r.URL.Query().Get("address")
+	network := r.URL.Query().Get("network")
+	if !strings.Contains(networks, network) || network == "" {
+		RenderJson(w, 400, J{"error": "invalid network"})
+		return
+	}
+	if len(address) == 0 {
+		RenderJson(w, 400, J{"error": "invalid address"})
+		return
+	}
+	users := DbSelect("select * from users where address_"+network+" = $1", address)
+	if len(users) > 0 {
+		user = users[0]
+	} else {
+		user["address_"+network] = address
+	}
+
+	if iphash != user.Get("iphash") {
+		db.MustExec(`insert into users_ips (id, user_id, iphash) values ($1, $2, $3)`, SUUID(), user.Get("id"), iphash)
+		user["iphash"] = iphash
+		refresh = true
+	}
+
+	if refresh || time.Now().Sub(user.GetTime("updated_at")) > 30*time.Minute {
+		fetchUpdateUserAmounts(user)
+		user["updated_at"] = time.Now()
+		db.MustExec(
+			`insert into users (id, address_ethereum, address_terra, address_fantom, address_polygon, amount_ethereum, amount_terra, amount_fantom, amount_polygon, amount_tclp, amount_forge, iphash, updated_at) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) on conflict (id) do update set address_ethereum = $2, address_terra = $3, address_fantom = $4, address_polygon = $5, amount_ethereum = $6, amount_terra = $7, amount_fantom = $8, amount_polygon = $9, amount_tclp = $10, amount_forge = $11, iphash = $12, updated_at = $13`,
+			user.Get("id"),
+			user.Get("address_ethereum"),
+			user.Get("address_terra"),
+			user.Get("address_fantom"),
+			user.Get("address_polygon"),
+			user.GetInt("amount_ethereum"),
+			user.GetInt("amount_terra"),
+			user.GetInt("amount_fantom"),
+			user.GetInt("amount_polygon"),
+			user.GetInt("amount_tclp"),
+			user.GetInt("amount_forge"),
+			user.Get("iphash"),
+			user.GetTime("updated_at"),
+		)
+	}
+
+	registrations := DbSelect("select * from users_registrations where user_id = $1", user.Get("id"))
+	RenderJson(w, 200, J{"user": user, "registrations": registrations})
+}
+
+func fetchUpdateUserAmounts(user J) {
+	// Ethereum
+	if address := user.Get("address_ethereum"); address != "" {
+		data, err := contractTiers.Pack("userInfoTotal", common.HexToAddress(address))
+		Check(err)
+		var resultStr string
+		err = client.Call(&resultStr, "eth_call", map[string]interface{}{
+			"from": ADDRESS_ZERO,
+			"to":   contractTiersAddress,
+			"data": hexutil.Bytes(data),
+		}, "latest")
+		if err == nil {
+			result, err := contractTiers.Unpack("userInfoTotal", hexutil.MustDecode(resultStr))
+			Check(err)
+			amountb := result[1].(*big.Int)
+			amountb.Div(amountb, big.NewInt(1000000000))
+			amountb.Div(amountb, big.NewInt(1000000000))
+			user["amount_ethereum"] = int(amountb.Int64())
+		} else {
+			log.Println("fetchUpdateUserAmounts: ethereum:", address, err)
+		}
+	}
+
+	// Terra
+	if address := user.Get("address_terra"); address != "" {
+		b64query := base64.URLEncoding.EncodeToString([]byte(`{"user_state":{"user":"` + address + `"}}`))
+		result, err := httpGet(`https://fcd.terra.dev/terra/wasm/v1beta1/contracts/terra18s7n93ja9nh37mttu66rhtsw05dxrcpsmw0c45/store?query_msg=` + b64query)
+		if err == nil {
+			state := result.(map[string]interface{})["query_result"].(map[string]interface{})
+			balance := big.NewInt(int64(MustParseInt(state["balance"].(string))))
+			balance.Div(balance, big.NewInt(1000000))
+			user["amount_terra"] = int(balance.Int64())
+		} else {
+			log.Println("fetchUpdateUserAmounts: terra:", address, err)
+		}
+	}
+
+	// TC LP
+	if address := user.Get("address_ethereum"); address != "" {
+		unitsStr := ""
+		poolName := "ETH.XRUNE-0X69FA0FEE221AD11012BAB0FDB45D444D3D2CE71C"
+		res, err := httpGet("https://midgard.thorchain.info/v2/member/" + address)
+		if err == nil {
+			for _, i := range res.(map[string]interface{})["pools"].([]interface{}) {
+				pool := i.(map[string]interface{})
+				if pool["pool"].(string) == poolName {
+					unitsStr = pool["liquidityUnits"].(string)
+				}
+			}
+		} else {
+			res, err = httpGet("https://multichain-asgard-consumer-api.vercel.app/api/v3/member/poollist?address=" + address)
+			if err == nil {
+				for _, i := range res.([]interface{}) {
+					pool := i.(map[string]interface{})
+					if pool["pool"].(string) == poolName {
+						unitsStr = pool["poolunits"].(string)
+					}
+				}
+			} else {
+				log.Println("fetchUpdateUserAmounts: tclp:", address, err)
+			}
+		}
+
+		if unitsStr != "" {
+			poolI, err := httpGet("https://midgard.thorchain.info/v2/pool/" + poolName)
+			if err == nil {
+				pool := J(poolI.(map[string]interface{}))
+				units := big.NewInt(int64(MustParseInt(unitsStr)))
+				unitsTotal := big.NewInt(int64(MustParseInt(pool.Get("liquidityUnits"))))
+				depth := big.NewInt(int64(MustParseInt(pool.Get("assetDepth"))))
+				units.Mul(units, depth)
+				units.Div(units, unitsTotal)
+				units.Div(units, big.NewInt(100000000))
+				user["amount_tclp"] = int(units.Int64())
+			} else {
+				log.Println("fetchUpdateUserAmounts: tclp:", err)
+			}
+		}
+	}
+}
+
+func handleUserRegister(w http.ResponseWriter, r *http.Request) {
+	ido := r.URL.Query().Get("ido")
+	userId := r.URL.Query().Get("user_id")
+	if !strings.Contains(allIdos, ido) {
+		RenderJson(w, 400, J{"error": "invalid ido"})
+		return
+	}
+	if len(userId) == 0 {
+		RenderJson(w, 400, J{"error": "invalid user_id"})
+		return
+	}
+	registrations := DbSelect("select * from users_registrations where user_id = $1 and ido = $2", userId, ido)
+	if len(registrations) == 0 {
+		db.MustExec("insert into users_registrations (id, ido, user_id) values ($1, $2, $3)", SUUID(), ido, userId)
+	}
+	RenderJson(w, 200, J{})
 }
 
 func handleStats(w http.ResponseWriter, r *http.Request) {
@@ -185,7 +249,7 @@ func handleStats(w http.ResponseWriter, r *http.Request) {
 		panic(fmt.Errorf("not a valid ido: " + ido))
 	}
 	stats := DbSelect(`select tier, sum(bonus) as bonus, count(id) as count from registrations where ido = $1 and bonus >= 0 and created_at <= $2 group by tier`, ido, idoCutoff[ido])
-	RenderJson(w, J{"stats": stats})
+	RenderJson(w, 200, J{"stats": stats})
 }
 
 func handleUser(w http.ResponseWriter, r *http.Request) {
@@ -194,7 +258,7 @@ func handleUser(w http.ResponseWriter, r *http.Request) {
 		address = common.HexToAddress(address).String()
 	}
 	registrations := DbSelect(`select ido, tier, created_at from registrations where address = $1`, address)
-	RenderJson(w, J{"registrations": registrations})
+	RenderJson(w, 200, J{"registrations": registrations})
 }
 
 func handleRegister(w http.ResponseWriter, r *http.Request) {
@@ -213,7 +277,7 @@ func handleRegister(w http.ResponseWriter, r *http.Request) {
 	bonus := MustParseInt(registration["bonus"].(string))
 	iphash := sha256.Sum256([]byte(ReqIP(r)))
 	if tier < 0 || tier > 5 {
-		RenderJson(w, J{"error": "invalid tier"})
+		RenderJson(w, 400, J{"error": "invalid tier"})
 		return
 	}
 
@@ -225,7 +289,7 @@ func handleRegister(w http.ResponseWriter, r *http.Request) {
 		db.MustExec(`insert into registrations (id, ido, address, tier, xrune, bonus, address_terra, iphash) values ($1, $2, $3, $4, $5, $6, $7, $8)`,
 			SUUID(), ido, account, tier, xrune, bonus, registration["terra"].(string), fmt.Sprintf("%x", iphash[:]))
 	}
-	RenderJson(w, J{"message": "ok"})
+	RenderJson(w, 200, J{"message": "ok"})
 }
 
 func handleKycStart(w http.ResponseWriter, r *http.Request) {
@@ -245,12 +309,12 @@ func handleKycStart(w http.ResponseWriter, r *http.Request) {
 			db.MustExec(`update kyc set session_id = $2 where id = $1`,
 				kyc[0]["id"].(string), sessionId)
 		}
-		RenderJson(w, J{"session_id": sessionId, "verified": resInfo["status"].(string) == "VERIFIED", "failed": failed})
+		RenderJson(w, 200, J{"session_id": sessionId, "verified": resInfo["status"].(string) == "VERIFIED", "failed": failed})
 	} else {
 		sessionId := synapsGetSessionId(address)
 		db.MustExec(`insert into kyc (id, address, session_id) values ($1, $2, $3)`,
 			SUUID(), address, sessionId)
-		RenderJson(w, J{"session_id": sessionId, "verified": false})
+		RenderJson(w, 200, J{"session_id": sessionId, "verified": false})
 	}
 }
 
@@ -429,4 +493,149 @@ func handleAdminSnapshot(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Fprintf(w, "%s,%s,%.2f,%d,%.2f\n", a["address"].(string), a["address_terra"].(string), a["xrune"].(float64), tier, allocation)
 	}
+}
+
+// UITLS
+///////////////////////////////////////////////////////////////
+
+func (j J) Get(k string) string {
+	if v, ok := j[k].(string); ok {
+		return v
+	}
+	return ""
+}
+
+func (j J) GetInt(k string) int {
+	if v, ok := j[k].(int); ok {
+		return v
+	}
+	return 0
+}
+
+func (j J) GetTime(k string) time.Time {
+	if v, ok := j[k].(time.Time); ok {
+		return v
+	}
+	return time.Time{}
+}
+
+func Check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Getenv(key string, alt string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return alt
+}
+
+func MustParseInt(s string) int {
+	i, err := strconv.Atoi(s)
+	Check(err)
+	return i
+}
+
+func SUUID() string {
+	u := [16]byte{}
+	_, err := rand.Read(u[:16])
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%x", u)
+}
+
+func RenderJson(w http.ResponseWriter, code int, v interface{}) {
+	bs, err := json.Marshal(v)
+	if err != nil {
+		RenderJson(w, 500, J{"error": err.Error()})
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(bs)
+}
+
+func ReqBody(r *http.Request, v interface{}) {
+	bs, err := io.ReadAll(r.Body)
+	Check(err)
+	r.Body.Close()
+	Check(json.Unmarshal(bs, &v))
+}
+
+func ReqIP(r *http.Request) string {
+	ip := r.Header.Get("X-REAL-IP")
+	nip := net.ParseIP(ip)
+	if nip != nil {
+		return ip
+	}
+
+	ips := r.Header.Get("X-FORWARDED-FOR")
+	sips := strings.Split(ips, ",")
+	for _, ip := range sips {
+		nip := net.ParseIP(ip)
+		if nip != nil {
+			return ip
+		}
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	Check(err)
+	nip = net.ParseIP(ip)
+	if nip != nil {
+		return ip
+	}
+	return ""
+}
+
+func DbSelect(sql string, args ...interface{}) []J {
+	log.Println("dbSelect:", sql)
+	results := []J{}
+	rows, err := db.Queryx(sql, args...)
+	Check(err)
+	for rows.Next() {
+		result := make(map[string]interface{})
+		Check(rows.MapScan(result))
+		results = append(results, result)
+	}
+	return results
+}
+
+func middleware(handler func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("request", r.Method, r.URL.String())
+		defer func() {
+			if err := recover(); err != nil {
+				fmt.Println("error", err)
+				RenderJson(w, 500, J{"error": fmt.Sprintf("%v", err)})
+			}
+		}()
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(200)
+			return
+		}
+		handler(w, r)
+	}
+}
+
+func httpGet(url string) (interface{}, error) {
+	log.Println("httpGet:", url)
+	res, err := http.DefaultClient.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode >= 400 {
+		return nil, fmt.Errorf("httpGet: error: %d: %s", res.StatusCode, string(body))
+	}
+	var v interface{}
+	err = json.Unmarshal(body, &v)
+	return v, err
 }
