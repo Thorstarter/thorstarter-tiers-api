5729f764c4e3c585a27c60655ef845c2fb13833f
kiasaki <kiasaki0000@gmail.com>
2022-02-21 10:31:07 -0500

    prepare detf

--- handle_snapshot.go
+++ handle_snapshot.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"sort"
 )
@@ -29,7 +28,7 @@ func snapshot(ido string, size float64) (float64, []J) {
 
 	totalAllocations := float64(0)
 	totalInTier := map[int]float64{}
-	//tierAllocations := map[int]float64{}
+	tierAllocations := map[int]float64{}
 	iphashes := map[string]int{}
 	filteredUsers := []J{}
 
@@ -37,76 +36,72 @@ func snapshot(ido string, size float64) (float64, []J) {
 		if user.GetInt("total") == 0 {
 			continue
 		}
-		if iphashes[user.Get("iphash")] >= 3 {
-			log.Println("user excluded ip", user.Get("user_id"))
-			continue
-		}
 		iphashes[user.Get("iphash")]++
 
-    /*
-    // UPDATE ALLOCATION
-    func() {
-      defer func() {
-        if err := recover(); err != nil {
-          log.Println("panic on user", user.Get("user_id"), err)
-        }
-      }()
-      r := user
-      us := DbSelect("select * from users where id = $1", r.Get("user_id"))
-      user := us[0]
-      fetchUpdateUserAmounts(user)
-      user["updated_at"] = time.Now()
-      db.MustExec(
-        `insert into users (id, address_ethereum, address_terra, address_fantom, address_polygon, amount_ethereum, amount_terra, amount_fantom, amount_polygon, amount_tclp, amount_forge, iphash, updated_at) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) on conflict (id) do update set address_ethereum = $2, address_terra = $3, address_fantom = $4, address_polygon = $5, amount_ethereum = $6, amount_terra = $7, amount_fantom = $8, amount_polygon = $9, amount_tclp = $10, amount_forge = $11, iphash = $12, updated_at = $13`,
-        user.Get("id"),
-        user.Get("address_ethereum"),
-        user.Get("address_terra"),
-        user.Get("address_fantom"),
-        user.Get("address_polygon"),
-        user.GetInt("amount_ethereum"),
-        user.GetInt("amount_terra"),
-        user.GetInt("amount_fantom"),
-        user.GetInt("amount_polygon"),
-        user.GetInt("amount_tclp"),
-        user.GetInt("amount_forge"),
-        user.Get("iphash"),
-        user.GetTime("updated_at"),
-      )
-      log.Println("done", i, "out of", len(users), r.Get("user_id"))
-    }()
-    /**/
+		/*
+		   // UPDATE ALLOCATION
+		   func() {
+		     defer func() {
+		       if err := recover(); err != nil {
+		         log.Println("panic on user", user.Get("user_id"), err)
+		       }
+		     }()
+		     r := user
+		     us := DbSelect("select * from users where id = $1", r.Get("user_id"))
+		     user := us[0]
+		     fetchUpdateUserAmounts(user)
+		     user["updated_at"] = time.Now()
+		     db.MustExec(
+		       `insert into users (id, address_ethereum, address_terra, address_fantom, address_polygon, amount_ethereum, amount_terra, amount_fantom, amount_polygon, amount_tclp, amount_forge, iphash, updated_at) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) on conflict (id) do update set address_ethereum = $2, address_terra = $3, address_fantom = $4, address_polygon = $5, amount_ethereum = $6, amount_terra = $7, amount_fantom = $8, amount_polygon = $9, amount_tclp = $10, amount_forge = $11, iphash = $12, updated_at = $13`,
+		       user.Get("id"),
+		       user.Get("address_ethereum"),
+		       user.Get("address_terra"),
+		       user.Get("address_fantom"),
+		       user.Get("address_polygon"),
+		       user.GetInt("amount_ethereum"),
+		       user.GetInt("amount_terra"),
+		       user.GetInt("amount_fantom"),
+		       user.GetInt("amount_polygon"),
+		       user.GetInt("amount_tclp"),
+		       user.GetInt("amount_forge"),
+		       user.Get("iphash"),
+		       user.GetTime("updated_at"),
+		     )
+		     log.Println("done", i, "out of", len(users), r.Get("user_id"))
+		   }()
+		   /**/
 
-    /*
-    // CHECK KYC
-		address := user.Get("address_ethereum")
-		if address == "" {
-			address = user.Get("address_terra")
-		}
-		if address == "" {
-			address = user.Get("address_fantom")
-		}
-		fmt.Println("fetching kyc", len(users), i+1)
-		kycVerified := false
-		sessions := DbSelect(`select id, session_id, verified from kyc where address = $1`, address)
-		for _, s := range sessions {
-			if s["verified"].(bool) {
-				kycVerified = true
-				continue
-			}
-			sessionId := s["session_id"].(string)
-			resInfo, err := synapsApiCall("GET", "/v3/session/info", sessionId)
-			if err == nil && resInfo["status"].(string) == "VERIFIED" {
-				kycVerified = true
-			}
-			if kycVerified {
-				db.MustExec(`update kyc set verified = true where id = $1`, s["id"].(string))
-			}
-		}
-		if !kycVerified {
-			fmt.Println("not kyced", user.Get("user_id"))
-			continue
-		}
-    /**/
+		/*
+			    // CHECK KYC
+					address := user.Get("address_ethereum")
+					if address == "" {
+						address = user.Get("address_terra")
+					}
+					if address == "" {
+						address = user.Get("address_fantom")
+					}
+					fmt.Println("fetching kyc", len(users), i+1)
+					kycVerified := false
+					sessions := DbSelect(`select id, session_id, verified from kyc where address = $1`, address)
+					for _, s := range sessions {
+						if s["verified"].(bool) {
+							kycVerified = true
+							continue
+						}
+						sessionId := s["session_id"].(string)
+						resInfo, err := synapsApiCall("GET", "/v3/session/info", sessionId)
+						if err == nil && resInfo["status"].(string) == "VERIFIED" {
+							kycVerified = true
+						}
+						if kycVerified {
+							db.MustExec(`update kyc set verified = true where id = $1`, s["id"].(string))
+						}
+					}
+					if !kycVerified {
+						fmt.Println("not kyced", user.Get("user_id"))
+						continue
+					}
+			    /**/
 
 		total := float64(user.GetInt("total"))
 		tier := int(0)
@@ -132,15 +127,16 @@ func snapshot(ido string, size float64) (float64, []J) {
 		tier := user.GetInt("tier")
 		allocation := float64(0)
 		allocation = baseAllocation * allMultipliers[tier]
-		user["possibleAllocation"] = allocation
-		// } else {
-		// 	tierAllocationCap := totalInTier[tier] * allMultipliers[tier] * baseAllocation
-		// 	if tierAllocations[tier]+100 < tierAllocationCap {
-		// 		allocation = 100
-		// 		tierAllocations[tier] += 100
-		// 	}
-		// 	user["possibleAllocation"] = float64(100)
-		// }
+		if allocation < 100 {
+			user["possibleAllocation"] = allocation
+		} else {
+			tierAllocationCap := totalInTier[tier] * allMultipliers[tier] * baseAllocation
+			if tierAllocations[tier]+100 < tierAllocationCap {
+				allocation = 100
+				tierAllocations[tier] += 100
+			}
+			user["possibleAllocation"] = float64(100)
+		}
 		user["allocation"] = allocation
 	}
 
